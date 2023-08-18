bddab3b603b0b3df5e797003cd1a26d74a650e01
Janett Gerrard <mikanchik@gmail.com>
2023-08-18 10:59:52 +0100

    connect stimulus controller to alerts

--- app/javascript/controllers/insert_alert_controller.js
+++ app/javascript/controllers/insert_alert_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-alert"
 export default class extends Controller {
+  static targets = ["container"]
+
   connect() {
+    console.log("hello from stimulus")
+    console.log(this.containerTarget)
   }
 }
