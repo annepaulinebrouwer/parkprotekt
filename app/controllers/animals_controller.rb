ddc175694b766a4c3a820c194eef2bee3f69439e
Janett Gerrard <mikanchik@gmail.com>
2023-08-19 10:55:00 +0100

    create marker info partial for info

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,7 +2,13 @@ class AnimalsController < ApplicationController
   before_action :set_animal, only: %i[show webcam]
 
   def show
-    @markers = @animal.markers
+    @markers = @animal.markers.map do |marker|
+      {
+        latitude: marker.latitude,
+        longitude: marker.longitude,
+        marker_info_html: render_to_string(partial: "marker_info", locals: {marker: marker})
+      }
+    end
   end
 
   def webcam
